a0103585d00b29dbc0435ec4d09dc8ecebb110ca
sohailshams <sohailshamas@yahoo.com>
2025-07-29 16:57:59 +0100

    Created TaskList component.

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,7 @@
 import { ref } from "vue";
 import TaskForm from "./components/TaskForm.vue";
 import type { Task } from "./types";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "./components/ui/card";
+import TaskList from "./components/TaskList.vue";
 
 const message = ref("Welcome to the Tasks App!");
 const tasks = ref<Task[]>([]);
@@ -26,13 +20,6 @@ const addTask = (newTask: string) => {
   <div>
     <h1 class="text-4xl font-bold my-6 text-center">{{ message }}</h1>
     <TaskForm @add-task="addTask" />
-    <div class="flex flex-wrap mx-auto max-w-[90%]">
-      <Card v-for="task in tasks" :key="task.id" class="my-4 w-32 mx-2">
-        <CardHeader>
-          <CardTitle>{{ task.title }}</CardTitle>
-          {{ task.completed ? "Completed" : "Pending" }}
-        </CardHeader>
-      </Card>
-    </div>
+    <TaskList :tasks />
   </div>
 </template>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { Task } from "@/types";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+
+const props = defineProps<{
+  tasks: Task[];
+}>();
+</script>
+
+<template>
+  <div class="flex flex-wrap mx-auto max-w-[90%]">
+    <Card v-for="task in props.tasks" :key="task.id" class="my-4 w-32 mx-2">
+      <CardHeader>
+        <CardTitle>{{ task.title }}</CardTitle>
+        {{ task.completed ? "Completed" : "Pending" }}
+      </CardHeader>
+    </Card>
+  </div>
+</template>
