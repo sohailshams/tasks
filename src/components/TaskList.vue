05748940718ee4d1165ace40ce6a09cd36df4199
sohailshams <sohailshamas@yahoo.com>
2025-07-29 22:37:56 +0100

    Added no task message and tasks completed message.

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -8,6 +8,12 @@ const props = defineProps<{
 </script>
 
 <template>
+  <p v-if="!props.tasks.length" class="text-center text-xl font-bold my-4">
+    No tasks available. Please add some tasks.
+  </p>
+  <p v-else class="text-center text-xl font-semibold my-4">
+    0 / {{ props.tasks.length }} tasks completed
+  </p>
   <div class="flex flex-wrap mx-auto max-w-[90%]">
     <Card v-for="task in props.tasks" :key="task.id" class="my-4 w-32 mx-2">
       <CardHeader>
